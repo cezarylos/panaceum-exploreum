f48e85228871b3d0eed684e77e7728f7c075b6c1
cezarylos <cezary.los@upflex.com>
2023-11-04 21:27:10 +0100

    feat(api): add basic api methods

---
+++ src/app/services/api/api.service.ts
@@ -0,0 +1,100 @@
+import {
+  AccountBalanceInterface,
+  NFTTransactionInterface,
+  TransactionInterface,
+  TransactionWrapperInterface,
+} from '@/app/services/api/typings';
+
+const cacheOptions = {
+  next: {
+    revalidate: 60,
+  },
+};
+
+const BASE_URL = process.env.NEXT_PUBLIC_ETHERSCAN_API_URL as string;
+
+const baseAccountParams = {
+  module: 'account',
+  apikey: process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY as string,
+};
+
+export class ApiService {
+  public static async GetEtherBalance(address: string): Promise<AccountBalanceInterface> {
+    const params = {
+      ...baseAccountParams,
+      action: 'balance',
+      address,
+      tag: 'latest',
+    };
+
+    try {
+      const res = await fetch(`${BASE_URL}?${new URLSearchParams(params).toString()}`, { ...cacheOptions });
+      return res.json();
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+
+  public static async getListOfTransactions({
+    address,
+    page = 1,
+    offset = 10,
+    sort = 'desc',
+  }: {
+    address: string;
+    page?: number;
+    offset?: number;
+    sort?: 'asc' | 'desc';
+  }): Promise<TransactionWrapperInterface<TransactionInterface>> {
+    const params = {
+      ...baseAccountParams,
+      action: 'txlist',
+      address,
+      startblock: '0',
+      endblock: '99999999',
+      sort,
+      page: page.toString(),
+      offset: offset.toString(),
+    };
+
+    try {
+      const res = await fetch(`${BASE_URL}?${new URLSearchParams(params).toString()}`, { ...cacheOptions });
+      return res.json();
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+
+  public static async getListOfNFTs({
+    address,
+    page = 1,
+    offset = 10,
+    sort = 'desc',
+  }: {
+    address: string;
+    page?: number;
+    offset?: number;
+    sort?: 'asc' | 'desc';
+  }): Promise<TransactionWrapperInterface<NFTTransactionInterface>> {
+    const params = {
+      ...baseAccountParams,
+      action: 'tokennfttx',
+      address,
+      startblock: '0',
+      endblock: '99999999',
+      sort,
+      page: page.toString(),
+      offset: offset.toString(),
+    };
+
+    try {
+      const res = await fetch(`${BASE_URL}?${new URLSearchParams(params).toString()}`, { ...cacheOptions });
+      return res.json();
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+}
---
+++ src/app/services/api/typings.ts
@@ -0,0 +1,45 @@
+export interface AccountBalanceInterface {
+  status: string;
+  message: string;
+  result: string;
+}
+
+export interface BaseTransactionInterface {
+  blockNumber: string;
+  timeStamp: string;
+  hash: string;
+  nonce: string;
+  blockHash: string;
+  from: string;
+  to: string;
+  value: string;
+  gas: string;
+  gasPrice: string;
+  isError: string;
+  input: string;
+  contractAddress: string;
+  cumulativeGasUsed: string;
+  gasUsed: string;
+  confirmations: string;
+}
+
+export interface TransactionInterface extends BaseTransactionInterface {
+  transactionIndex?: string;
+  txreceipt_status: string;
+  methodId: string;
+  functionName: string;
+}
+
+export interface NFTTransactionInterface extends BaseTransactionInterface {
+  contractAddress: string;
+  tokenID: string;
+  tokenName: string;
+  tokenSymbol: string;
+  tokenDecimal: string;
+}
+
+export interface TransactionWrapperInterface<T> {
+  status: string;
+  message: string;
+  result: T[];
+}
